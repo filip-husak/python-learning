847a917f709a2454cb28203fe29fd7df9663787b
filip-husak <filip.husak70@gmail.com>
2026-08-02 17:23:15 +0000

    Travel Weather Planner project done, with objectives.

---
+++ Travel Weather Planner/travel_weather_planner.py
@@ -0,0 +1,18 @@
+### Exercise: Travel Weather Planner (conditionals) - w/o any notes or examples
+
+distance_mi = 0
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+elif distance_mi <= 1 and not is_raining:
+    print(True)
+elif distance_mi > 1 and distance_mi <= 6 and has_bike and not is_raining:
+    print(False)
+elif distance_mi > 6 and (has_car or has_ride_share_app):
+    print(True)
+else:
+    print(False)
